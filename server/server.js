06b95952a4338a221ea269f51a21686a22fcb50e
= <jo123.garcia.s@gmail.com>
2018-10-07 20:19:03 -0500

    testing body-parser

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/user', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/user', (req, res) => {
+  const { body } = req;
+  res.json({
+    body,
+  });
 });
 
 app.listen(3000, () => {
