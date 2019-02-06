5d8c47ba5c3a65a0f82dc6623e194b920d671734
= <jo123.garcia.s@gmail.com>
2019-02-06 13:28:20 -0600

    initial post route for login

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,9 +6,30 @@ const User = require('../models/user');
 
 
 router.post('/', (request, response) => {
-  response.json({
-    ok: true,
-  });
+  const { body } = request;
+
+  User.findOne({ email: body.email })
+    .then((user) => {
+      if (!user || !bcrypt.compareSync(body.password, user.password)) {
+        return response.status(400).json({
+          ok: false,
+          error: {
+            message: 'Incorrect password or user ',
+          },
+        });
+      }
+
+      response.json({
+        ok: true,
+        user,
+        token: '123',
+      });
+    }).catch((error) => {
+      response.status(500).json({
+        ok: false,
+        error,
+      });
+    });
 });
 
 module.exports = router;
